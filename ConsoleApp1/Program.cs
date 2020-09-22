65429952f864e868b1ba6424c7ec56bce404efaa
kastetbro <kastet59gmail.com>
2020-09-22 20:34:03 +0300

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        private static int BinarySearch(ref int[] array, int valueToFind)
+        {
+            int middle;
+            int middleNumber;
+            int begin = 0;
+            int end = array.Length - 1;
+            while(!(begin > end))
+            {
+                middle = (begin + end) / 2;
+                middleNumber = array[middle];
+                if (middleNumber == valueToFind)
+                {
+                    return middleNumber;
+                }
+                else if (middleNumber > valueToFind)
+                {
+                    end = middle - 1;
+                }
+                else if (middleNumber < valueToFind)
+                {
+                    begin = middle + 1;
+                }
+            }
+            throw new ArgumentException("No such number.");
+        }
+        static void Main(string[] args)
+        {
+            int[] array = {1, 2, 3, 4, 5 ,6};
+            Console.WriteLine(BinarySearch(ref array, 1));
+            Console.WriteLine(BinarySearch(ref array, 5));
+            Console.WriteLine(BinarySearch(ref array, 6));
+            Console.WriteLine(BinarySearch(ref array, 7));
+        }
+    }
+}
